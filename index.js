b0fcea9863facb5f3123f022d56b8ac560d8e0f4
Whitey <github@and.rew.ninja>
2023-07-13 23:04:44 +1000

    Fixed permission checking, made channel loops work, added inclusions and exclusions, added dataset file writing

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const discord = new Client({
 	// All partials are loaded automatically
 });
 const config = require('config')
+const fs = require('fs')
 
 discord.on('ready', async () => {
   console.log(`${discord.user.username} is ready!`);
@@ -18,19 +19,39 @@ async function fetchAllMessages() {
   let davidMessages = [];
   let dataset = [];
   //console.dir(channel, { depth: null });
+  const guild = discord.guilds.cache.get(config.get('discord.guildId'));
 
-  discord.guilds.cache.get(config.get('guildId')).channels.cache.forEach(async (channel) => {
+  for (let ch of guild.channels.cache) {
+    // ch[0] is the channel id, ch[1] is the object
+    let channel = ch[1]
     if (channel.type === 'GUILD_TEXT'){
-      console.log(`Looping through ${channel.name}`)
+      if (!channel.permissionsFor(discord.user.id).has(['READ_MESSAGE_HISTORY'])) {
+        //console.log(`I don't have permission to read ${channel.name}`)
+        continue;
+      }
+      if (config.get('discord.excludedChannelIds').includes(channel.id)) {
+        //console.log(`${channel.name} is on my exclusion list`)
+        continue;
+      }
+      if (config.get('discord.includedChannelIds').length > 0) {
+        if (!config.get('discord.includedChannelIds').includes(channel.id)) {
+          //console.log(`We're on included channel IDs only, and ${channel.name} isn't one of them`)
+          continue;
+        }
+      }
+      console.log(`----- Getting messages for ${channel.name}...`)
       // Create message pointer
       let message = await channel.messages
-        .fetch({ limit: 1 })
-        .then(messagePage => (messagePage.size === 1 ? messagePage.at(0) : null));
+      .fetch({ limit: 1 })
+      .then(messagePage => (messagePage.size === 1 ? messagePage.at(0) : null));
+
+      let loopCount = 0
 
       while (message) {
         await channel.messages
           .fetch({ limit: 100, before: message.id })
           .then(async (messagePage) => {
+            loopCount += messagePage.size
             messagePage.forEach(async (msg) => {
               // messages[messageId] = {
               //   'timestamp': msg.createdTimestamp,
@@ -41,7 +62,7 @@ async function fetchAllMessages() {
               //   'isReply': (msg.type == 'REPLY') ? true : false,
               //   'replyMessageId': (msg.reference) ? msg.reference.messageId : ''
               // };
-              if (msg.author.id == config.get('davidId') && msg.type == 'REPLY') {
+              if (msg.author.id == config.get('discord.davidId') && msg.type == 'REPLY') {
                 //console.log(`Found a match (${msg.author.id} == ${davidId} and it's a ${msg.type})`)
                 let replyMessage = await channel.messages.fetch(msg.reference.messageId)
                 davidMessages.push({
@@ -62,14 +83,17 @@ async function fetchAllMessages() {
 
           })
         if (message) {
-          console.log(`Found ${davidMessages.length} matches...`)
+          console.log(`Found ${davidMessages.length} matches in ${loopCount} messages...`)
           let seconds = Math.floor(Math.random() * (7500 - 3700) + 3500);
           console.log(`Waiting ${Math.floor(seconds/1000)} seconds before continuing...`)
           await new Promise(resolve => setTimeout(resolve, seconds));
         }
+        else {
+          loopCount = 0
+        }
       }
     }
-  });
+  };
 
   //console.log(davidMessages);  // Print all messages
   if (davidMessages.length < 1) {
@@ -89,12 +113,20 @@ async function fetchAllMessages() {
       "output": msg.content
     })
   })
-  console.log(dataset);
+  //console.log(dataset);
+  try {
+    let d = new Date()
+    let filename = `${d.getFullYear()}${(d.getMonth()+1).toString().padStart(2, '0')}${d.getDate()}${d.getHours().toString().padStart(2, '0')}${d.getMinutes().toString().padStart(2, '0')}${d.getSeconds().toString().padStart(2, '0')}`
+    fs.writeFileSync(`./generated-datasets/${filename}.json`, dataset);
+    console(`Wrote dataset to ./generated-datasets/${filename}.json`)
+  } catch (err) {
+    console.error(err);
+  }
 }
 
 // main
 async function main() {
-    discord.login(config.get('discordToken'));
+    discord.login(config.get('discord.token'));
 }
 
 main()
